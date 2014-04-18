a54a0a9cd36753361a617d95e4dd2aa3bd63b384
Eric Abrahamsen <eric@ploonky.com>
2014-04-18 17:44:32 -0400

    -Cleaned up test cases -Simplified CSVtoJSON class to increase readability -Made CSVtoJSON runable

--- CSVtoJSON.rb
+++ CSVtoJSON.rb
@@ -1,53 +1,114 @@
-# CSVtoJSON
+require 'logger'
+require 'JSON'
+
+class String
+  def numeric?
+    return true if self =~ /^\d+$/
+    true if Float(self) rescue false
+  end
+end
+
 class CSVtoJSON
   MODIFIER_NAME = "modifier"
 
-  def keys(keys)
-    return keys.split(',')
-  end
-
-  def defineBaseObject(keys)
-    #this method defines the empty base object structure which can be
-    #traversed later loading data
-    @baseObject = {}
-    @keys.each do |key|
-      findModifier = key.match(/#{MODIFIER_NAME}(_)(\d+)(_)/)
-      if findModifier
-	#is a modifier field
-        modifier=key[findModifier.offset(0)[1]..-1]
-        previousMod=@baseObject[MODIFIER_NAME] 
-	modVal = previousMod.append({modifier=>nil})
-        @baseObject.merge!(modVal)
-      else
-        @baseObject.merge!({key=>nil})
-      end
-    end
-    return @baseObject
+  def initialize
+    logFile = File.open('csvtojson.log')
+    @logger = Logger.new logFile
   end
 
   def parse(file_contents)
-    #inputs: file_contents is the contents of the CSV file without keys
-    #outputs: ruby array containing a representation of the CSV file
+    #input: 
+    #        file_content is the content of the CSV file
+    #output: ruby array containing a JSON representation of the CSV file
     parsedArray = []
     lines = file_contents.split("\n")
+    keyArr = keys(lines.first)
+    lines=lines[1..-1]
     lines.each do |line|
-      itemDict = {}
-      values=line.split(",")
-      values.each_with_index do |index,value|
-	key = @keys[index]
-        modifier = key.match(/(_)(\d+)(_)/)
-        field = {}
-	#currently handles a single depth of modifier
-        if modifier
-          modFieldName = key[modifier.offset(0)[1]..-1]
-          if field.class==Hash
-	    field=[]
-	  end
-	  field.append({modFieldName=>value})
+      begin
+        itemDict = {}
+        values=line.split(",")
+        values.each_with_index do |value, index|
+          value=sanitizeInputs(value)
+          key = keyArr[index]
+          findModifier = key.match(/#{MODIFIER_NAME}(_)(\d+)(_)/)
+          if findModifier
+            modifier=key[findModifier.offset(0)[1]..-1]
+            modifierIndex = findModifier[2].to_i-1
+            modifierHash = itemDict[MODIFIER_NAME] ? {MODIFIER_NAME=>itemDict[MODIFIER_NAME]} : nil
+            itemDict.merge!(parseModifierField(modifier,value,modifierIndex,modifierHash))
+          else
+            itemDict[key]=value
+          end
         end
-
-	   
+        parsedArray.push(itemDict)
+      rescue Exception => e
+	puts "Error: #{e} on the following line - #{line}"
+	@logger.error "#{e} on the following line - #{line}"
       end
     end
+    return parsedArray
+  end
+private
+  def sanitizeInputs(value)
+    value = value.strip
+    if value.empty?
+      #empty values should be nil instead of an empty string
+      return nil                     
+    end
+
+    if value.start_with?("$")
+      #money is formatted as decimal, drop $ symbol
+      return value[1..value.size].to_f
+    end
+
+    if value.to_i.to_s==value
+      #check to see if numbers can be converted to integers
+      return value.to_i
+    end
+
+    if value.numeric?
+      #check to see if numbers can be converted to floats
+      return value.to_f
+    end
+
+    return value
+  end
+  def parseModifierField(modifierKey,modifierValue,modifierIndex,modifierHash)
+    if !modifierHash
+      modifierHash = {MODIFIER_NAME=>[]}
+    end 
+    if modifierHash[MODIFIER_NAME][modifierIndex]
+      #Add field to existing modifier item
+      modifierHash[MODIFIER_NAME][modifierIndex].merge!({modifierKey => modifierValue})
+    else
+      #Add field to new modifier item
+      modifierHash[MODIFIER_NAME].push({modifierKey => modifierValue})
+    end
+    return modifierHash
+  end
+  def keys(keys)
+    keyArr=keys.split(',').map{|a| a.strip}
+    return keyArr
+  end
+end
+
+if __FILE__ == $0
+  while (true)
+    ctj=CSVtoJSON.new
+    filesToProcess = Dir.entries("./toProcess")
+    #remove current dir and parent dir
+    filesToProcess = filesToProcess.select{|file| file.match(/\.csv/)}
+
+    filesToProcess.each do |filename|
+      filePath = "./toProcess/#{filename}"
+      file = File.open(filePath)
+      parsedData = ctj.parse(file.read)
+      jsonFile= File.write("./done/#{filename}.json", parsedData.to_json)
+      File.rename(filePath,"./done/#{filename}")
+    end
+    #set the frequency to check for file updates
+    #In a production system make this a rake task and schedule a cron job
+    sleep(1)
   end
 end
--- CSVtoJSONTest.rb
+++ CSVtoJSONTest.rb
@@ -4,54 +4,75 @@ shared_context "shared_test_data" do
   before do
     begin
       @file = File.open('test_data.csv','r')
-      @keys = file.first
+      @keys = @file.first
     rescue
       #TODO: Be more specific about the error types
       puts "Test file doesn't exist or is empty"
     end
-    @sample_parsed = [
-	             {
-	               "id"=> 111010,
-		       "description"=> "Coffee",
-                       "price"=> 1.25,
-		       "cost"=> 0.8,
-		       "price_type"=> "system",
-	               "quantity_on_hand"=> 100000,
-                       "modifier"=> [
-                         {
-                           "name"=> "Small",
-                           "price"=> -0.25
-                         },
-                         {
-                           "name"=> "Medium",
-                           "price"=> 0
-                         },
-                         {
-                           "name"=> "Large",
-                           "price"=> 0.3
-                         }
-                       ]
-                     } 
-                   ]    
+    @sample_parsed = {
+	               "id"=>111010,
+		       "description"=>"Coffee",
+		       "price"=>1.25,
+		       "cost"=>0.8,
+		       "price_type"=>"system",
+		       "quantity_on_hand"=>100000,
+		       "modifier"=>[
+			       {"name"=>"Small",
+			        "price"=>"-$0.25"},
+			       {"name"=>"Medium",
+			        "price"=>0.0},
+			       {"name"=>"Large",
+			        "price"=>0.3}
+                               ]
+                       }
   end
 end
 
+describe CSVtoJSON, "#sanitizeInputs" do
+  it "check for money" do
+    ctj = CSVtoJSON.new
+    t1 = ctj.send(:sanitizeInputs,"$2.43")
+    t2 = ctj.send(:sanitizeInputs,"$3.00")
+    expect(t1).to eq(2.43)
+    expect(t2).to eq(3.00)
+  end
+  it "check for floats" do
+    ctj = CSVtoJSON.new
+    t1 = ctj.send(:sanitizeInputs,"2.43")
+    t2 = ctj.send(:sanitizeInputs,"3.00")
+    expect(t1).to eq(2.43)
+    expect(t2).to eq(3.00)
+  end
+  it "check for integers" do
+    ctj = CSVtoJSON.new
+    t1 = ctj.send(:sanitizeInputs,"2")
+    t2 = ctj.send(:sanitizeInputs,"3")
+    expect(t1).to eq(2)
+    expect(t2).to eq(3)
+  end
+  it "check for empty strings" do
+    ctj = CSVtoJSON.new
+    t1 = ctj.send(:sanitizeInputs,"")
+    expect(t1).to eq(nil)
+  end
+end
 
 describe CSVtoJSON, "#keys" do include_context "shared_test_data"
 
   it "sets keys of JSON file" do
     ctj = CSVtoJSON.new
+    t1 = ctj.send(:keys,@keys)
     expect(["id","description","price","cost","price_type","quantity_on_hand",
 	   "modifier_1_name","modifier_1_price","modifier_2_name","modifier_2_price",
-	   "modifier_3_name","modifier_3_price"]).to eq(ctj.keys(@keys))
+	   "modifier_3_name","modifier_3_price"]).to eq(t1)
   end
 end
 
 describe CSVtoJSON, "#parse" do include_context "shared_test_data"
   it "convert to ruby dictionary and array" do
      ctj = CSVtoJSON.new
-     ctj.keys=@keys
-     result = ctj.parse(@file)
-     expect([result[0]]).to eq(@sample_parsed)
+     result = ctj.parse(@keys+@file.read)
+     puts result
+     expect(result[0]).to eq(@sample_parsed)
   end
 end
